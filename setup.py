97b8c4d905c4f69d2c25aa8d4080c5594e42892e
Redmoogle <dakotamew@gmail.com>
2021-04-12 18:09:02 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     url='https://github.com/redmoogle/jsonreader',
     packages=setuptools.find_packages(),
     install_requires=[
-        'ujson'
+        'orjson'
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
