3c5791d0b129a64f4892017bb7fdc5a5ebe91e8e
Redmoogle <dakotamew@gmail.com>
2021-04-12 17:56:33 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,6 +13,9 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url='https://github.com/redmoogle/jsonreader',
     packages=setuptools.find_packages(),
+    install_requires=[
+        'ujson'
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         'License :: OSI Approved :: MIT License',
