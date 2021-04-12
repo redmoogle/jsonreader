e5b6dc5fc6c1347dea0f7d481a4a60b25ef7ef2a
Redmoogle <dakotamew@gmail.com>
2021-04-12 18:15:06 -0400

    Update jsonreader.py

--- guildreader/jsonreader.py
+++ guildreader/jsonreader.py
@@ -2,20 +2,18 @@
 handles json files
 """
 
-import orjson as json
+import ujson as json
 from pathlib import Path
 
 
 def create_file(bot, key: str, default, wipe: bool = False):
     """
     Creates a JSON config file for all guilds it can see with a default
-
         Parameters:
             bot (discord.Bot): Reference to the bot so it can get the guilds
             key (str): The `guild_key.json` to generate
             default (any): The value to put in as a placeholder
             wipe (bool): Wipe the files forcefully
-
         Returns:
             Success (bool): Did it succeed
     """
@@ -36,11 +34,9 @@ def create_file(bot, key: str, default, wipe: bool = False):
 def read_file(guild, key: str):
     """
     Reads a JSON file given the ID to find and key to look in
-
         Parameters:
             guild (str/int): the ID of the guild to return given key
             key (str): The key to look up
-
         Returns:
             data(any): The data for that guild and key
     """
@@ -59,12 +55,10 @@ def read_file(guild, key: str):
 def write_file(guild, key: str, value):
     """
     Writes data to a guild JSON given a key
-
         Parameters:
             guild (str/int): ID of the guild
             key (str): The key to modify
             value (any): The value to write for that guild and key
-
         Returns:
             Success (bool): Did it succeed
     """
@@ -87,11 +81,9 @@ def write_file(guild, key: str, value):
 def remove(guild, key: str):
     """
     Removes a guild from a given key
-
         Parameters:
             guild (str/int): ID of the guild
             key (str): The key to modify
-
         Returns:
             Success (bool): Did it succeed
     """
@@ -115,10 +107,8 @@ def remove(guild, key: str):
 def check_exist(key: str):
     """
     Checks if a JSON key has been generated yet
-
         Parameters:
             key (str): The key to check
-
         Returns:
             Exist (bool): Does it exist
     """
@@ -128,10 +118,8 @@ def check_exist(key: str):
 def dump(key: str):
     """
     Dumps all the JSON given the key
-
         Parameters:
             key (str): The key to dump
-
         Returns:
             data (dict): The JSON data
     """
