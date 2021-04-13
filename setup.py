86b62fb655740d7ac9d6a7d587a41e7f3d7891e1
redmoogle <dakotamew@gmail.com>
2021-04-13 10:59:45 -0400

    Minor Improvements

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as file:
 
 setuptools.setup(
     name='guildreader',
-    version='1.1.1',
+    version='1.1.2',
     author='Redmoogle',
     author_email='dakotamew@gmail.com',
     description='DiscordPy Guild Data Management Module',
