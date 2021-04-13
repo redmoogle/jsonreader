569af91c3b013b0dbdef214d12357d4fcac3856e
Redmoogle <dakotamew@gmail.com>
2021-04-13 07:03:16 -0400

    Increment for test

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as file:
 
 setuptools.setup(
     name='guildreader',
-    version='1.1.0',
+    version='1.1.1',
     author='Redmoogle',
     author_email='dakotamew@gmail.com',
     description='DiscordPy Guild Data Management Module',
