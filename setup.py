97095afaaf8aa19a8aeace8dc13e66551b793869
Redmoogle <dakotamew@gmail.com>
2021-04-13 12:40:38 -0400

    [1.1.3] Improves readme with examples (#6)
    * Improvements to readme
    
    * bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as file:
 
 setuptools.setup(
     name='guildreader',
-    version='1.1.2',
+    version='1.1.3',
     author='Redmoogle',
     author_email='dakotamew@gmail.com',
     description='DiscordPy Guild Data Management Module',
