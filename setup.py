668ef8948e5e2f567578f94c8b4c4c4d800303a1
Redmoogle <dakotamew@gmail.com>
2022-04-05 13:07:59 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as file:
 
 setuptools.setup(
     name='guildreader',
-    version='2.0.0',
+    version='2.0.1',
     author='Redmoogle',
     author_email='dakotamew@gmail.com',
     description='JSON Data Management Module',
